e8bc8a97fa5a067974e0deb77c90645eebce1a3b
ankaj89 <45896847+ankaj89@users.noreply.github.com>
2018-12-16 13:12:04 +0530

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -3,7 +3,7 @@ package runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features="src/test/resources/features", glue="steps", tags={"@selenium"}, format={"html:Reports"})
+@CucumberOptions(features="src/test/resources/features", glue="steps", tags={"@selenium"}, format={"json:report.json"})
 public class Runner extends AbstractTestNGCucumberTests{
 
 }
